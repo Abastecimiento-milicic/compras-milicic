6f2faf0a272f835af8f4da0788d495c94875e190
malenasalguero <malena.salguero@milicic.com.ar>
2026-08-05 17:29:51 -0300

    Update last-update.js

--- last-update.js
+++ last-update.js
@@ -4,7 +4,7 @@
 window.FECHA_ULTIMA_ACTUALIZACION = "03/08/2026";   //CAMBIAR FECHA
 
 // 2. Clave de versión global (Fallback seguro siempre)
-window.MI_CACHE_VERSION = window.FECHA_ULTIMA_ACTUALIZACION.replace(/\//g, "") || "20260804";   //CAMBIAR FECHA
+window.MI_CACHE_VERSION = window.FECHA_ULTIMA_ACTUALIZACION.replace(/\//g, "") || "20260805";   //CAMBIAR FECHA
 
 // Dibuja la fecha automáticamente al cargar
 document.addEventListener("DOMContentLoaded", () => {
